cf51a32cc900ec6c7520f98c77e9561e620bc23e
Luckyfans <497620104@qq.com>
2018-09-13 20:23:17 +0800

    First commit

---
+++ demo/src/main/java/com/example/arcpaint/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.arcpaint;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=(Button) findViewById(R.id.add);
+    }
+}
